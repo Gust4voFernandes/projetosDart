1b26961bf81549fe9c9bc09359fcdb4c37b931d1
Gustavo Fernandes Gonçalves de Lima <15937447654@CAMPUS.MACHADO.MCH>
2024-03-14 09:03:55 -0300

    first commit

--- SegundaLista_14_03_2024/ex07.dart
+++ SegundaLista_14_03_2024/ex07.dart
@@ -4,6 +4,16 @@ void main(){
   var n = int.parse(stdin.readLineSync()!);
 
   if(n % 2 == 0 && n % 3 == 0){
-    
+    print("Par e Divisivel po 3");
   }
+
+  if(n % 2 == 0){
+    print("Apenas Par");
+  }else if(n % 3 == 0){
+    print("Apenas Divisivel por 3");
+  }else{
+    print("Não é par nem divisivel por 3");
+  }
+
+
 }
